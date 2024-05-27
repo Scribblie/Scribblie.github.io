8a3065aa14d20ac86807165e19517e8160a5a123
Scribblie <aspenvanveldhoven@gmail.com>
2024-05-27 21:10:18 +0200

    Add text transition to dates

--- src/lib/Yarn.svelte
+++ src/lib/Yarn.svelte
@@ -165,5 +165,6 @@
         color: black;
         font-family: "Courier New", Courier, monospace;
         text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+        transition: font-size 3s ease-in-out;
     }
 </style>
